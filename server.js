f5a3c55fdbe6d09c969c614a83f184ea0da77d21
Max <madmaxlt97@gmail.com>
2025-04-28 20:46:58 +0300

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
