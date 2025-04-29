10582d6174976dbd12f838e9b6a10b8a9380c13f
Max <madmaxlt97@gmail.com>
2025-04-29 22:05:22 +0300

    test

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
